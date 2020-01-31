1495235c853b1e0f3445babbec5c4488175d6a57
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 16:56:48 -0500

    Fun Project!

--- App/src/main/java/com/API/App/model/RecruiterInfo.java
+++ App/src/main/java/com/API/App/model/RecruiterInfo.java
@@ -1,4 +1,10 @@
 package com.API.App.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class RecruiterInfo {
 }
