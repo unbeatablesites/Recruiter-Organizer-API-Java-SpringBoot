7fef0fd383adcfedefed7fd0abc46dcb99c1b0c1
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 17:00:59 -0500

    Fun Project!

--- App/src/main/java/com/API/App/model/RecruiterInfo.java
+++ App/src/main/java/com/API/App/model/RecruiterInfo.java
@@ -3,8 +3,19 @@ package com.API.App.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class RecruiterInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer recruiterId;
+    private String recruiterName;
+    private String companyName;
+
+
 }
