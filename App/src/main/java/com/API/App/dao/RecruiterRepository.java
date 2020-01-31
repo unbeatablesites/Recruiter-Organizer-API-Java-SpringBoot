2cd7d9a50193d5258f5d7550f94c77ba17551009
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 17:21:45 -0500

    Fun Project!

--- App/src/main/java/com/API/App/dao/RecruiterRepository.java
+++ App/src/main/java/com/API/App/dao/RecruiterRepository.java
@@ -1,4 +1,16 @@
 package com.API.App.dao;
 
-public interface RecruiterRepository {
+
+import com.API.App.model.RecruiterInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RecruiterRepository  extends JpaRepository<RecruiterInfo, Integer> {
+
+
+    List<RecruiterInfo> findByFirstName(String recruiterFirstName);
+
 }
