87bf2d8bb78835acca68121283999eef95a0651d
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 17:07:50 -0500

    Fun Project!

--- App/src/main/java/com/API/App/model/RecruiterInfo.java
+++ App/src/main/java/com/API/App/model/RecruiterInfo.java
@@ -22,6 +22,67 @@ public class RecruiterInfo {
     private Boolean interested;
     private String notes;
 
+    public Integer getRecruiterId() {
+        return recruiterId;
+    }
 
+    public void setRecruiterId(Integer recruiterId) {
+        this.recruiterId = recruiterId;
+    }
 
+    public String getRecruiterFirstName() {
+        return recruiterFirstName;
+    }
+
+    public void setRecruiterFirstName(String recruiterFirstName) {
+        this.recruiterFirstName = recruiterFirstName;
+    }
+
+    public String getRecruiterLastName() {
+        return recruiterLastName;
+    }
+
+    public void setRecruiterLastName(String recruiterLastName) {
+        this.recruiterLastName = recruiterLastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getRecruiterPhoneNumber() {
+        return recruiterPhoneNumber;
+    }
+
+    public void setRecruiterPhoneNumber(String recruiterPhoneNumber) {
+        this.recruiterPhoneNumber = recruiterPhoneNumber;
+    }
+
+    public LocalDate getDateLastSpokeTo() {
+        return dateLastSpokeTo;
+    }
+
+    public void setDateLastSpokeTo(LocalDate dateLastSpokeTo) {
+        this.dateLastSpokeTo = dateLastSpokeTo;
+    }
+
+    public Boolean getInterested() {
+        return interested;
+    }
+
+    public void setInterested(Boolean interested) {
+        this.interested = interested;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
