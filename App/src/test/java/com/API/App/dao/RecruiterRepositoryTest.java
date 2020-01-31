f415bbc2b67a591bdbb8e607f4c88acd447b98c4
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 17:56:50 -0500

    Fun Project!

---
+++ App/src/test/java/com/API/App/dao/RecruiterRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.API.App.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecruiterRepositoryTest {
+
+
+    @Autowired
+    RecruiterRepository recruiterRepository;
+
+    @Test
+    void findByrecruiterFirstName() {
+    }
+
+    @Test
+    void findByrecruiterPhoneNumber() {
+    }
+
+    @Test
+    void findBycompanyName() {
+    }
+}
