930cb44b543234c5d62c87161fb85a563ea0ed56
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 17:29:11 -0500

    Fun Project!

--- App/src/test/java/com/API/App/AppApplicationTests.java
+++ App/src/test/java/com/API/App/AppApplicationTests.java
@@ -1,10 +1,16 @@
 package com.API.App;
 
+import com.API.App.dao.RecruiterRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 class AppApplicationTests {
+
+	@Autowired
+	RecruiterRepository recruiterRepository;
 
 	@Test
 	void contextLoads() {
