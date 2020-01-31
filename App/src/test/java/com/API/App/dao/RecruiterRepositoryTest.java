bed21d30914e958b319193fa75fa6e10c61f2aec
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 18:05:27 -0500

    Fun Project!

--- App/src/test/java/com/API/App/dao/RecruiterRepositoryTest.java
+++ App/src/test/java/com/API/App/dao/RecruiterRepositoryTest.java
@@ -2,9 +2,10 @@ package com.API.App.dao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class RecruiterRepositoryTest {
 
 
