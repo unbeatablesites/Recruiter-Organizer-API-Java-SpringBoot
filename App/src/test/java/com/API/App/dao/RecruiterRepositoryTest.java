afedf27d150b456799dc573ce55534d48521d7a4
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 18:58:09 -0500

    Fun Project!

--- App/src/test/java/com/API/App/dao/RecruiterRepositoryTest.java
+++ App/src/test/java/com/API/App/dao/RecruiterRepositoryTest.java
@@ -50,7 +50,7 @@ class RecruiterRepositoryTest {
         assertEquals(2, listOfRec.size());
 
         List<RecruiterInfo> listOfRec2 = recruiterRepository.findByrecruiterFirstName("Carl");
-            assertEquals(0, listOfRec2.size());
+            assertEquals(1, listOfRec2.size());
 
         List<RecruiterInfo> listOfRec3 = recruiterRepository.findByrecruiterFirstName("Mike");
         assertEquals(false, listOfRec.contains("Mike"));
