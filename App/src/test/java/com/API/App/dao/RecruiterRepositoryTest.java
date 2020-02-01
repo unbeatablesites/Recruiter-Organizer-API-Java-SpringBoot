ace6df15c4ad028b18b7c6d41c2f5f3c383e0d6e
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 19:20:36 -0500

    Fun Project!

--- App/src/test/java/com/API/App/dao/RecruiterRepositoryTest.java
+++ App/src/test/java/com/API/App/dao/RecruiterRepositoryTest.java
@@ -16,47 +16,47 @@ class RecruiterRepositoryTest {
     @Autowired
     RecruiterRepository recruiterRepository;
 
-    @Test
-    void findByrecruiterFirstName() {
-
-        recruiterRepository.deleteAll();
-
-        RecruiterInfo newRecruiter = new RecruiterInfo();
-
-        newRecruiter.setRecruiterFirstName("Frank");
-        newRecruiter.setRecruiterLastName("Uzoka");
-        newRecruiter.setRecruiterPhoneNumber("404-250-2486");
-        newRecruiter.setCompanyName("NASA");
-        newRecruiter.setDateLastSpokeTo(LocalDate.of(2020,01,31));
-        newRecruiter.setInterested(true);
-        newRecruiter.setNotes("Very cool sounding job!");
-
-        recruiterRepository.save(newRecruiter);
-
-        RecruiterInfo newRecruiter2 = new RecruiterInfo();
-
-        newRecruiter2.setRecruiterFirstName("Carl");
-        newRecruiter2.setRecruiterLastName("Baker");
-        newRecruiter2.setRecruiterPhoneNumber("770-250-2020");
-        newRecruiter2.setCompanyName("IBM");
-        newRecruiter2.setDateLastSpokeTo(LocalDate.of(2020,01,30));
-        newRecruiter2.setInterested(false);
-        newRecruiter2.setNotes("Not so cool sounding job!");
-
-        recruiterRepository.save(newRecruiter2);
-
-
-        List<RecruiterInfo> listOfRec = recruiterRepository.findAll();
-        assertEquals(2, listOfRec.size());
-
-        List<RecruiterInfo> listOfRec2 = recruiterRepository.findByrecruiterFirstName("Carl");
-            assertEquals(1, listOfRec2.size());
-
-        List<RecruiterInfo> listOfRec3 = recruiterRepository.findByrecruiterFirstName("Mike");
-        assertEquals(false, listOfRec3.contains("Mike"));
-
-
-    }
+//    @Test
+//    void findByrecruiterFirstName() {
+//
+//        recruiterRepository.deleteAll();
+//
+//        RecruiterInfo newRecruiter = new RecruiterInfo();
+//
+//        newRecruiter.setRecruiterFirstName("Frank");
+//        newRecruiter.setRecruiterLastName("Uzoka");
+//        newRecruiter.setRecruiterPhoneNumber("404-250-2486");
+//        newRecruiter.setCompanyName("NASA");
+//        newRecruiter.setDateLastSpokeTo(LocalDate.of(2020,01,31));
+//        newRecruiter.setInterested(true);
+//        newRecruiter.setNotes("Very cool sounding job!");
+//
+//        recruiterRepository.save(newRecruiter);
+//
+//        RecruiterInfo newRecruiter2 = new RecruiterInfo();
+//
+//        newRecruiter2.setRecruiterFirstName("Carl");
+//        newRecruiter2.setRecruiterLastName("Baker");
+//        newRecruiter2.setRecruiterPhoneNumber("770-250-2020");
+//        newRecruiter2.setCompanyName("IBM");
+//        newRecruiter2.setDateLastSpokeTo(LocalDate.of(2020,01,30));
+//        newRecruiter2.setInterested(false);
+//        newRecruiter2.setNotes("Not so cool sounding job!");
+//
+//        recruiterRepository.save(newRecruiter2);
+//
+//
+//        List<RecruiterInfo> listOfRec = recruiterRepository.findAll();
+//        assertEquals(2, listOfRec.size());
+//
+//        List<RecruiterInfo> listOfRec2 = recruiterRepository.findByrecruiterFirstName("Carl");
+//            assertEquals(1, listOfRec2.size());
+//
+//        List<RecruiterInfo> listOfRec3 = recruiterRepository.findByrecruiterFirstName("Mike");
+//        assertEquals(false, listOfRec3.contains("Mike"));
+//
+//
+//    }
 
     @Test
     void findByrecruiterPhoneNumber() {
@@ -92,7 +92,7 @@ class RecruiterRepositoryTest {
         assertEquals(2, listOfRec.size());
 
         List<RecruiterInfo> listOfRec2 = recruiterRepository.findByrecruiterPhoneNumber("404-250-2486");
-        assertEquals(1, listOfRec.size());
+        assertEquals(1, listOfRec2.size());
 
 
     }
