d3e9522e6fa86d1dddf376b296d0f3d174c5f35b
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 19:39:47 -0500

    Fun Project!

--- App/src/main/java/com/API/App/controller/RecruiterController.java
+++ App/src/main/java/com/API/App/controller/RecruiterController.java
@@ -1,4 +1,24 @@
 package com.API.App.controller;
 
+import com.API.App.dao.RecruiterRepository;
+import com.API.App.model.RecruiterInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recruiters")
 public class RecruiterController {
+
+    @Autowired
+    RecruiterRepository recruiterRepository;
+
+    @PostMapping("/add")
+    public RecruiterInfo createRecruiter(@RequestBody RecruiterInfo recruiterInfo){
+        return recruiterRepository.save(recruiterInfo);
+    }
+
+
 }
